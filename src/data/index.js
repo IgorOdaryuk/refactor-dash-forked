b0988befa3e96eafc00412eccf79aa14fdb87fcc
IgorOdaryuk <odaryukigor@gmail.com>
2026-06-30 15:20:29 +0200

    chore: add July data file (empty) + wire into index — ready for July collection

--- src/data/index.js
+++ src/data/index.js
@@ -3,5 +3,6 @@ import { febData } from "./feb";
 import { marData } from "./mar";
 import { aprData } from "./apr";
 import { junData } from "./jun";
+import { julData } from "./jul";
 
-export const dailyData = [...janData, ...febData, ...marData, ...aprData, ...junData];
+export const dailyData = [...janData, ...febData, ...marData, ...aprData, ...junData, ...julData];
